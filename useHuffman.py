b8f0152bc6b690ca5f15ebfae26f8e8882b2b510
Mosam Patel <51872350+patelmosam@users.noreply.github.com>
2019-12-15 13:31:22 +0530

    file uploaded

---
+++ useHuffman.py
@@ -0,0 +1,12 @@
+from huff2 import Huffman
+import sys
+
+path = 'images\\sample.bmp'
+
+h = Huffman(path)
+
+output_path = h.compress()
+print("Compressed file path: " + output_path)
+
+decom_path = h.decompress(output_path)
+print("Decompressed file path: " + decom_path)
